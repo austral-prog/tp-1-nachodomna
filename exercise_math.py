48652bf9fbf689e428f443f91232eb0b15c0a507
nachodomna <idomnanovich@mail.austral.edu.ar>
2025-03-27 16:29:37 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -2,15 +2,15 @@ def math():
     a = 57
     b = 7
     print(a + b)
-	print(a - b)
-	print(a * b)
-	c = a + b
-	d = 2
-	z1 = c / d
-	print(z1)
-	f1 = a//b
-	print(f1)
-	f2 = a % b
-	print(f2)
-	f3 = a / b
-	print(f3)
+    print(a - b)
+    print(a * b)
+    c = a + b
+    d = 2
+    z1 = c / d
+    print(z1)
+    f1 = a//b
+    print(f1)
+    f2 = a % b
+    print(f2)
+    f3 = a / b
+    print(f3)
